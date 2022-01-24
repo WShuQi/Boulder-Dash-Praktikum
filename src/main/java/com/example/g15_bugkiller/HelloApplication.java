b48b016f5f9d9b905673b5172646cfb82312632e
Charis Noreen Thron <thron@cip.ifi.lmu.de>
2022-01-24 11:16:44 +0100

    replaced hello-view.fxml with Pane

--- src/main/java/com/example/g15_bugkiller/HelloApplication.java
+++ src/main/java/com/example/g15_bugkiller/HelloApplication.java
@@ -3,8 +3,10 @@ package com.example.g15_bugkiller;
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,12 +16,12 @@ import org.json.JSONObject;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-       /* FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Pane pane = new Pane();
+        Scene scene = new Scene(pane, 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
-*/
+
         // for testing
         Level level = TestInputData.createLevelData();
         KeyPressListener keyPressListener = new KeyPressListener();
