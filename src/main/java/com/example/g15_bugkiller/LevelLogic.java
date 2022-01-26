5ad34d74b719a1e20bb557ebf55b18c213ebdc57
Stefan Haffner <haffner@cip.ifi.lmu.de>
2022-01-26 18:50:09 +0100

    comments in LevelLogic deleted

--- src/main/java/com/example/g15_bugkiller/LevelLogic.java
+++ src/main/java/com/example/g15_bugkiller/LevelLogic.java
@@ -362,30 +362,4 @@ public class LevelLogic {
         }
     }
 
-
-
-// Methoden für Hauptregeln
-
-    // Die Spielfigur kann sich bewegen
-    // Gegner bewegen sich
-    // Verschiedene Gegenstände fallen nach unten
-    // Herunterfallende Gegenstande können Gegner sowie die Spielfigur erschlagen
-    // Schleim breitet sich aus
-    // Edelsteine können eingesammelt werden
-    // Hat man genügend Edelsteine gesammelt, so kann man ein Level durch einen Ausgang verlassen
-
-
-
-
-        /*
-        public static void printTypes (Field[][] map) {
-
-            for(int y=0; map[y].length>y; y++){
-                for (int x=0; map[x].length>x; x++){
-
-                        System.out.println(map[y][x].getType());
-                }
-            }
-        }
-        */
 }
